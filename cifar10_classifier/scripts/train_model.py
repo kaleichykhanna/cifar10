d574d753f2d008e131241ce48ce7b216460dec89
kaleichykhanna <kaleychik.anna@mail.ru>
2025-01-17 19:56:02 +0300

    trained the model

--- cifar10_classifier/scripts/train_model.py
+++ cifar10_classifier/scripts/train_model.py
@@ -1,8 +1,7 @@
 from torch.utils.data import DataLoader
 
-def train_loop(dataloader: DataLoader, model, loss_fn, optimizer, print_every = 100):
+def train_loop(dataloader: DataLoader, model, loss_fn, optimizer, print=True, print_every = 100):
     model.train()
-    size = len(dataloader.dataset)
 
     for batch, (X, y) in enumerate(dataloader):
         pred = model(X)
@@ -12,6 +11,7 @@ def train_loop(dataloader: DataLoader, model, loss_fn, optimizer, print_every = 
         optimizer.step()
         optimizer.zero_grad()
 
-        if batch % print_every == 0:
-            loss = loss.item()
-            print(f"loss: {loss:>7f} batch: {batch:>5d}")
+        if print:
+            if batch % print_every == 0:
+                loss = loss.item()
+                print(f"loss: {loss:>7f} batch: {batch:>5d}")
