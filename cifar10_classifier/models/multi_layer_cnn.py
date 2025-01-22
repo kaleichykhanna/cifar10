2bbed788bdcf065d7cb9f0120d3ef5b1dbfac1a0
kaleichykhanna <kaleychik.anna@mail.ru>
2025-01-22 16:47:25 +0300

    created a multi-layer cnn trained on the whole dataset

--- cifar10_classifier/models/multi_layer_cnn.py
+++ cifar10_classifier/models/multi_layer_cnn.py
@@ -1,18 +1,30 @@
+import torch
 import torch.nn as nn
 
-class MulyiLayerNet(nn.Module):
+class MultiLayerCnn(nn.Module):
     def __init__(self):
         super().__init__()
-        self.multi_layer_stack = nn.Sequential(
+        
+        self.multi_layer_cnn_stack = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),  
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
             nn.Flatten(), 
-            nn.Linear(32*32*3, 256),
+
+            nn.Linear(128 * 4 * 4, 512),
             nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Linear(64, 10),
+
+            nn.Linear(512, 10)
         )
+    
     def forward(self, x):
-        logits = self.one_layer_stack(x)
-        return logits
+        return self.multi_layer_cnn_stack(x)
